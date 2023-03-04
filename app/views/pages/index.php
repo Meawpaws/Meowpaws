6d9255c98d5c1a99147cf22a57e72a7520726ee4
Marouane Bouchettoy <uanemaro216@gmail.com>
2023-03-04 19:10:53 +0100

    DASHBOARD

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,11 @@
-<?php include_once APPROOT . '/views/inc/navbarUser.inc.php' ?>
-<img src="" alt="cover">
-<?php include_once APPROOT . '/views/inc/footer.inc.php' ?>
+<?php include_once APPROOT . '/views/inc/navbarUser.inc.php'?>
+<img class="coverImage" src="<?=URLROOT?>image/siteWebPages/c-1.svg" alt="cover">
+<img class="coverImage" src="<?=URLROOT?>image/siteWebPages/c-2.svg" alt="cover">
+<div class="h1">
+    <h1>
+        <img class="iconImage" src="<?=URLROOT?>image/siteWebPages/i-1.svg" alt="cover">
+        Top Trending For This Season
+        <img class="iconImage" src="<?=URLROOT?>image/siteWebPages/i-2.svg" alt="cover">
+    </h1>
+</div>
+<?php include_once APPROOT . '/views/inc/footer.inc.php'?>
