365d0e3463b89da7a6910acb6e76bb5836188a1a
Marouane Bouchettoy <uanemaro216@gmail.com>
2023-03-19 11:09:48 +0000

    finish models

--- backend/app/models/CommentStar/CommentStar.class.php
+++ backend/app/models/CommentStar/CommentStar.class.php
@@ -6,7 +6,14 @@ class CommentStar
     {
         $this->db = new Database;
     }
-    public function add($comment,$star,$id_u,$id_p)
+    public function addImage($id_cs,$image)
+    {
+        $this->db->query('INSERT INTO `picturescommentstar`(`image`, `id_cs`) VALUES (:image,:id_cs)');
+        $this->db->bind(':id_cs',$id_cs);
+        $this->db->bind(':image',$image);
+        $this->db->execute();
+    }
+    public function add($comment,$star,$id_u,$id_p,$image = [])
     {
         $this->db->query('INSERT INTO `commentstar`(`comment`, `star`, `id_u`, `id_p`) VALUES (:comment,:star,:id_u,:id_p)');
         $this->db->bind(':comment',$comment);
@@ -14,13 +21,18 @@ class CommentStar
         $this->db->bind(':id_u',$id_u);
         $this->db->bind(':id_p',$id_p);
         $this->db->execute();
+
+        $this->db->query("SELECT * FROM `commentstar` ORDER BY `commentstar`.`id_cs` DESC LIMIT 1");
+        $row = $this->db->fetch();
+        $id_cs = $row->id_cs;
+
+        for ($i=0; $i < count($image); $i++) { 
+            $this->addImage($id_cs,$image[$i]);
+        }
     }
-    public function update($comment,$star,$id_cs)
+    public function update($sql)
     {
-        $this->db->query('UPDATE `commentstar` SET `comment`=:comment,`star`=:star WHERE `id_cs`= :id_cs');
-        $this->db->bind(':comment',$comment);
-        $this->db->bind(':star',$star);
-        $this->db->bind(':id_cs',$id_cs);
+        $this->db->query($sql);
         if ($this->db->execute())
             return true;
         else
@@ -37,13 +49,13 @@ class CommentStar
     }
     public function selectAll()
     {
-        $this->db->query("SELECT * FROM `category`");
+        $this->db->query("SELECT * FROM `commentstar` cs, `users` us, `product` po WHERE cs.id_p = po.id_p AND cs.id_u = us.id_u");
         $row = $this->db->fetchAll();
         return $row;
     }
     public function selectById($id)
     {
-        $this->db->query("SELECT * FROM `category` WHERE `id_c` = :id");
+        $this->db->query("SELECT * FROM `commentstar` cs, `users` us, `product` po  WHERE cs.id_p = po.id_p AND cs.id_u = us.id_u AND cs.id_cs = :id");
         $this->db->bind(':id',$id);
         $row = $this->db->fetch();
         return $row;
