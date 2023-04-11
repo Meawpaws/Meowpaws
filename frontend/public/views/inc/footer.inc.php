2a0f762fe30fb22a1d7f6f5e4078b9519ba14e1c
marouane <uanemaro216@gmail.com>
2023-04-11 16:19:08 +0000

    update

--- frontend/public/views/inc/footer.inc.php
+++ frontend/public/views/inc/footer.inc.php
@@ -65,6 +65,7 @@
 <!-- script -->
 <script src=" <?=URLROOT?>/layout/js/main.js"></script>
 <script src="<?=URLROOT?>/layout/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
 </body>
 
--- frontend/public/views/inc/header.inc.php
+++ frontend/public/views/inc/header.inc.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="<?=URLROOT?>layout/css/responsive.css">
     <link rel="stylesheet" href="<?=URLROOT?>layout/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?=URLROOT?>layout/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <title><?=APPNAME?></title>
 </head>
