3880848b7f932ebd1ba7ba6a0107fa682dfcfbae
Marouane Bouchettoy <uanemaro216@gmail.com>
2023-04-15 22:55:05 +0000

    update

--- frontend/public/layout/js/addItem.js
+++ frontend/public/layout/js/addItem.js
@@ -1,43 +1,30 @@
-(function () {
-  ("use strict");
-  const forms = document.querySelectorAll(".requires-validation");
-  Array.from(forms).forEach(function (form) {
-    form.addEventListener(
-      "click",
-      function (event) {
-        localStorage.setItem("checkClickUserEdit", 0);
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
+// const categorySelect = document.getElementById('categorySelect')
 
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
+// fetch(`http://localhost/meowpaws/backend/Admins/getCategories`, {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json"
+//   }
+// })
+//   .then((res) => res.json())
+//   .then((data) => {
+//     if (data.message == 'Categories Isset') {
+//       var categories = data.result
+//       for (let i = 0; i < categories.length; i++) {
+//         const id_c = categories[i].id_c
+//         const cname = categories[i].cname
+//         var option = document.createElement("option");
+//         option.value = id_c;
+//         option.text = cname;
+//         categorySelect.appendChild(option);
+//       }
+//     }
+//   })
 
-const categorySelect = document.getElementById('categorySelect')
-
-fetch(`http://localhost/meowpaws/backend/Admins/getCategories`, {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json"
-  }
-})
-  .then((res) => res.json())
-  .then((data) => {
-    if (data.message == 'Categories Isset') {
-      var categories = data.result
-      for (let i = 0; i < categories.length; i++) {
-        const id_c = categories[i].id_c
-        const cname = categories[i].cname
-        var option = document.createElement("option");
-        option.value = id_c;
-        option.text = cname;
-        categorySelect.appendChild(option);
-      }
-    }
-  })
-
+//   function clickInInput(input){
+//     const inputFile = document.getElementById(`inputImage${input}`)
+//     console.log(inputFile);
+//     inputFile.click()
+//   }
+  
+//   const addUser = document.getElementById('addUser')
