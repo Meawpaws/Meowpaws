9f1b0a3ba19b101ba0dde735509acd42f3fac194
Marouane Bouchettoy <uanemaro216@gmail.com>
2023-04-16 01:29:21 +0000

    update

--- frontend/public/layout/js/addItem.js
+++ frontend/public/layout/js/addItem.js
@@ -1,30 +1,35 @@
-// const categorySelect = document.getElementById('categorySelect')
+const categorySelect = document.getElementById('categorySelect')
 
-// fetch(`http://localhost/meowpaws/backend/Admins/getCategories`, {
-//   method: "GET",
-//   headers: {
-//     "Content-Type": "application/json"
-//   }
-// })
-//   .then((res) => res.json())
-//   .then((data) => {
-//     if (data.message == 'Categories Isset') {
-//       var categories = data.result
-//       for (let i = 0; i < categories.length; i++) {
-//         const id_c = categories[i].id_c
-//         const cname = categories[i].cname
-//         var option = document.createElement("option");
-//         option.value = id_c;
-//         option.text = cname;
-//         categorySelect.appendChild(option);
-//       }
-//     }
-//   })
+fetch(`http://localhost/meowpaws/backend/Admins/getCategories`, {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.message == 'Categories Isset') {
+      var categories = data.result
+      for (let i = 0; i < categories.length; i++) {
+        const id_c = categories[i].id_c
+        const cname = categories[i].cname
+        var option = document.createElement("option");
+        option.value = id_c;
+        option.text = cname;
+        categorySelect.appendChild(option);
+      }
+    }
+  })
 
-//   function clickInInput(input){
-//     const inputFile = document.getElementById(`inputImage${input}`)
-//     console.log(inputFile);
-//     inputFile.click()
-//   }
-  
-//   const addUser = document.getElementById('addUser')
+  function checkFiles(input) {
+    if (input.files.length > 4) {
+      input.value = '';
+      alert('Please select no more than 4 files.');
+    }
+  }
+
+  const addItem = document.getElementById('addItem')
+
+  addItem.addEventListener('submit',(event)=>{
+    event.preventDefault
+  })
--- frontend/public/views/admin/itemAdd.php
+++ frontend/public/views/admin/itemAdd.php
@@ -1,5 +1,39 @@
 <?php include_once './views/inc/header.inc.php' ?>
 <?php include_once './views/inc/navbarAdmin.inc.php' ?>
 
+<link rel="stylesheet" href="<?=URLROOT?>layout/css/addForm.css">
+
+<form class="addItemForm" id="addItem">
+    <div class="addItemDiv">
+        <label for="name" class="addItemLabel">Name</label>
+        <input type="text" class="addItemInput" name="name" id="name" required>
+    </div>
+    <div class="addItemDiv">
+        <label for="price" class="addItemLabel">Price</label>
+        <input type="number" class="addItemInput" name="price" id="price" required>
+    </div>
+    <div class="addItemDiv">
+        <label for="description" class="addItemLabel">Description</label>
+        <textarea class="addItemInput addItemTextarea" name="description" id="description" required></textarea>
+    </div>
+    <div class="addItemDiv">
+        <label for="categorySelect" class="addItemLabel">Category</label>
+        <select name="category" class="addItemInput addItemSelect" id="categorySelect" required>
+            <option value="" selected>Category</option>
+        </select>
+    </div>
+    <div class="addItemDiv">
+        <label for="imagePrincipal" class="addItemLabel addItemLabelImage">Image Principal</label>
+        <input type="file" class="addItemInput addItemImage" name="imagePrincipal" id="imagePrincipal" accept="image/*" required>
+    </div>
+    <div class="addItemDiv">
+        <label for="imageSeconder" class="addItemLabel addItemLabelImage">Images Seconders <span class="addItemSpan">Max 4 Images</span></label>
+        <input type="file" class="addItemInput addItemImage" name="imageSeconder" id="imageSeconder" accept="image/*" multiple onchange="checkFiles(this)">
+    </div>
+    <div class="addItemDiv">
+        <input type="submit" class="addItemInput addItemSubmit" value="Save">
+    </div>
+</form>
+
 <?php include_once './views/inc/footer.inc.php' ?>
 <script src='<?= URLROOT ?>layout/js/addItem.js'></script>
